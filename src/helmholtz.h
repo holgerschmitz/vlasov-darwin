0a24821d62c4a076d362eb199013619eff133960
Holger Schmitz <holger@notjustphysics.com>
2003-09-09 13:00:08 +0000

    Added source documentation

--- src/helmholtz.h
+++ src/helmholtz.h
@@ -5,6 +5,10 @@
 
 /** @brief Solves Helmholtz equation using a multigrid method.
  *
+ *  The Helmholtz equation is given by
+ *  \f$\Delta u(x) -\lambda(x)u(x) = f(x)\f$
+ *  where \f$u(x)\f$ is the unknown scalar field.
+ *
  *  Assumes that arrays range from 0 to N+1, where 0 and N+1 are
  *  periodic boundaries.
  */
@@ -21,39 +25,71 @@ class Helmholtz {
         /// The error to reach.
         double epsilon;
     public:
+        /** @brief The default constructor reads the attributes from
+         *  global variables
+         */
         Helmholtz();
+    
+        /** @brief Solves the Helmholtz equation
+         *  \f$\Delta u(x) -\lambda(x)u(x) = f(x)\f$.
+         *  The solution is returned in the first parameter.
+         */
         void solve(   NumMatrix<double,2> &u,
 	                  NumMatrix<double,2> &f,
                       NumMatrix<double,2> &lambda);
 
+        /// The grid spacing in x-direction
         double Dx() { 
             return dx[0];
         }
+        
+        /// The grid spacing in y-direction
         double Dy() { 
             return dx[1];
         }
     private:
+        /// Perform one Gauss-Seidel red-black iteration
         void gauss( NumMatrix<double,2> &u,     
 	                NumMatrix<double,2> &f,
                     NumMatrix<double,2> &lambda);   
                      
+        /** @brief Calculates the defect and stores the new
+         *  fields of \f$f(x)\f$ and \f$\lambda(x)\f$ on the
+         *  coarser grid
+         */
         void defect(NumMatrix<double,2> &u,
                     NumMatrix<double,2> &f,
                     NumMatrix<double,2> &lambda,
                     NumMatrix<double,2> &fn,
                     NumMatrix<double,2> &lambdan);
-                                      
+                        
+        
+        /** @brief Prolongates the solution \f$u(x)\f$ onto 
+         *  the finer grid resolution.
+         */                              
         void prolongate(NumMatrix<double,2> &u,
 	                    NumMatrix<double,2> &un);
         
+        /** @brief One single multigrid step.
+         *  This method calls itself recursively on coarser
+         *  and coarser grids
+         */
         void mgi(   NumMatrix<double,2> &u,     
 	                NumMatrix<double,2> &f,
                     NumMatrix<double,2> &lambda);    
-                    
+        
+        /** @brief Normalizes a scalar field.
+         *  This method is not needed for the Helmhotz solver.
+         */            
         void normalize( NumMatrix<double,2> &f);
-                
+        
+        /// Calculates the maximum norm between two scalar fields
         double distance(NumMatrix<double,2> &u,
 	                    NumMatrix<double,2> &f);
-                        
+        
+        /** @brief Performs periodic wrapping of the boundaries
+         *  @todo This is geometry dependent and should be supplied
+         *  by a special boundary object
+         */
         void boundary(NumMatrix<double,2> &u);
 };
