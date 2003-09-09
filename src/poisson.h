6603cf9594a232c1334dbd151b45e1f7ff177c4e
Holger Schmitz <holger@notjustphysics.com>
2003-09-09 14:41:56 +0000

    Updated source documentation

--- src/poisson.h
+++ src/poisson.h
@@ -16,31 +16,64 @@
  *  See the page @ref hwscrt
  */
 class Poisson {
-    private:
-        /// The physical coordinates of the minimum and maximum points
-        PositionD pMiN, pMaX;
-        /// The type of boundary condition for the two directions
-        PositionI boundary;
-        /// A temporary working array needed by hwscrt
-        double *WorkArray;
-    public:
-        enum bcond { periodic=0, fixfix=1, fixder=2, derfix=3, derder=4 };
-        Poisson();
-        Poisson(const PositionD &pMiN_, const PositionD &pMaX_, 
-                const PositionI &steps_,
-                bcond boundx, bcond boundy);
-        ~Poisson();
-        void resize(const PositionD &pMiN_, const PositionD &pMaX_, 
-                    const PositionI &steps_,
-                    bcond boundx, bcond boundy);
-        void resize(const PositionD &pMiN_, const PositionD &pMaX_, 
-                    const PositionI &steps_);
-        void BCond(bcond boundx, bcond boundy);
-        void solve(ScalarField &In, ScalarField &Out);
-    private:
-        void solve_step(ScalarField &In, ScalarField &Out);
-        void do_resize(const PositionD &pMiN_, const PositionD &pMaX_, 
-                       const PositionI &steps_);       
+  private:
+      /// The physical coordinates of the minimum and maximum points
+      PositionD pMiN, pMaX;
+      /// The type of boundary condition for the two directions
+      PositionI boundary;
+      /// A temporary working array needed by hwscrt
+      double *WorkArray;
+  public:
+      /** @brief An enumeration that contains the type of boundary conditions.
+       */
+      enum bcond { periodic=0, fixfix=1, fixder=2, derfix=3, derder=4 };
+  
+      /// Default constructor
+      Poisson();
+      
+      /** @brief Construct using the physical extent, the number of
+       *  steps in each direction and the boundary conditions
+       */
+      Poisson(const PositionD &pMiN_, const PositionD &pMaX_, 
+              const PositionI &steps_,
+              bcond boundx, bcond boundy);
+      
+      /// Destruct and free the WorkArray buffer
+      ~Poisson();     
+      
+      /** @brief Resize the physical extent, the number of
+       *  steps in each direction and the boundary conditions
+       */
+      void resize(const PositionD &pMiN_, const PositionD &pMaX_, 
+                  const PositionI &steps_,
+                  bcond boundx, bcond boundy);
+      
+      /** @brief Resize the physical extent, the number of
+       *  steps in each direction. Don't change the boundary
+       *  conditions
+       */
+      void resize(const PositionD &pMiN_, const PositionD &pMaX_, 
+                  const PositionI &steps_);
+      
+      /// Set new boundary conditions
+      void BCond(bcond boundx, bcond boundy);
+      
+      /** @brief Solve the Poisson equation.
+       *  The result is returned in the second parameter. Internally the
+       *  solve_step method is called twice to promote the second
+       *  order hwscrt to a fourth order solver
+       */
+      void solve(ScalarField &In, ScalarField &Out);
+  private:
+      
+      /// Perform a single call to hwscrt and perform wrapping
+      void solve_step(ScalarField &In, ScalarField &Out);
+  
+      /** @brief Perform resizing. Set the parameters and allocate
+       *  memory for the WorkArray
+       */
+      void do_resize(const PositionD &pMiN_, const PositionD &pMaX_, 
+                     const PositionI &steps_);       
 };
 
 #endif
