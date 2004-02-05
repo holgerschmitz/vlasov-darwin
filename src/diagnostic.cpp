8b07978aaef62627965c2de6ab309eab3bea8123
Holger Schmitz <holger@notjustphysics.com>
2004-02-05 09:49:13 +0000

    Bugfix in DiagnosticInterface. Output was only created for t=0

--- src/diagnostic.cpp
+++ src/diagnostic.cpp
@@ -15,11 +15,12 @@ void DiagnosticInterface::execute() {
   if (singleOut() && !(BoundaryKeeper::getBoundary()->master()) ) return;
   
   if ((0==t) && appending()) open(fname);
-  if ( (t % interval) != 0 ) return;
-  
-  if (!appending()) open(parsedFileName());
-  write();
-  if (!appending()) close();
+  if ( (t % interval) == 0 )
+  {
+    if (!appending()) open(parsedFileName());
+    write();
+    if (!appending()) close();
+  }
   ++t;
 }
 
